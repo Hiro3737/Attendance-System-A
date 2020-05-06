371d4a8e99321d1fcd2a3f5e67159d2d59f3d4da
Hiroki37 <hiroki10037@gmail.com>
2020-05-06 16:11:31 +0900

    指定時間のデフォルト直設定

--- db/migrate/20200506055915_add_employee_number_to_user.rb
+++ db/migrate/20200506055915_add_employee_number_to_user.rb
@@ -2,8 +2,8 @@ class AddEmployeeNumberToUser < ActiveRecord::Migration[5.1]
   def change
     add_column :users, :employee_number, :integer, comment:"ユーザーの社員番号"
     add_column :users, :uid, :string, comment:"ユーザーのカード番号"
-    add_column :users, :designated_working_start, :datetime, comment:"ユーザーの指定勤務開始時間"
-    add_column :users, :designated_work_end_time, :datetime, comment:"ユーザーの指定勤務終了時間"
+    add_column :users, :designated_working_start, :datetime, default: Time.current.change(hour: 9, min: 0, sec: 0), comment:"ユーザーの指定勤務開始時間"
+    add_column :users, :designated_work_end_time, :datetime, default: Time.current.change(hour: 18, min: 0, sec: 0), comment:"ユーザーの指定勤務終了時間"
     add_column :users, :superion, :boolean, default: false, comment:"上長かどうか"
   end
 end
