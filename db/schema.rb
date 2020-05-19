03be41f55760a6bdc3c8c39a3892e40482dbbf90
Hiroki37 <hiroki10037@gmail.com>
2020-05-19 17:30:08 +0900

    migration:resetした

--- db/schema.rb
+++ db/schema.rb
@@ -32,12 +32,12 @@ ActiveRecord::Schema.define(version: 20200506055915) do
     t.string "remember_digest"
     t.string "affiliation"
     t.boolean "admin", default: false
-    t.datetime "basic_time", default: "2020-05-13 23:00:00"
-    t.datetime "work_time", default: "2020-05-13 22:30:00"
+    t.datetime "basic_time", default: "2020-05-17 23:00:00"
+    t.datetime "work_time", default: "2020-05-17 22:30:00"
     t.integer "employee_number"
     t.string "uid"
-    t.datetime "designated_work_start_time", default: "2020-05-14 00:00:00"
-    t.datetime "designated_work_end_time", default: "2020-05-14 09:00:00"
+    t.datetime "designated_work_start_time", default: "2020-05-18 00:00:00"
+    t.datetime "designated_work_end_time", default: "2020-05-18 09:00:00"
     t.boolean "superion", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
